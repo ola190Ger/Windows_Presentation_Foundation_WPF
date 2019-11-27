02d70299a0f4cf4e516f8b480584431f05f073aa
ola190Ger <49449139+ola190Ger@users.noreply.github.com>
2019-11-27 22:01:51 +0300

    add

---
+++ dz1_3_убегающая кнока/dz1_3_убегающая кнока/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dz1_3_убегающая_кнока
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.GetPosition(null).X > 0 && e.GetPosition(null).X < btn.Margin.Left + 10) && (e.GetPosition(null).Y > 0 && e.GetPosition(null).Y < btn.Margin.Top + 10+btn.ActualHeight))
+            {
+                //e.GetPosition(null) = new Point(e.GetPosition(null).X + rnd.Next(-10, 10), e.GetPosition(null).Y + rnd.Next(-10, 10));
+                btn.Margin = new Thickness(btn.Margin.Left + 10, btn.Margin.Top, 0, 0);
+            }
+            // /*
+            else if ((e.GetPosition(null).X <= 0 || e.GetPosition(null).X >= btn.Margin.Left + 10) && (e.GetPosition(null).Y > 0 && e.GetPosition(null).Y < btn.Margin.Top + 10+btn.ActualHeight))
+            {
+                //label1.Location = new Point(e.GetPosition(null).X, e.GetPosition(null).Y + rnd.Next(-10, 10));
+                btn.Margin = new Thickness(btn.Margin.Left, btn.Margin.Top + 10, 0, 0);
+
+            }
+            else
+            if ((e.GetPosition(null).X > 0 && e.GetPosition(null).X < btn.Margin.Left - 10) && (e.GetPosition(null).Y <= 0 || e.GetPosition(null).Y >= btn.Margin.Top + 10+btn.ActualHeight))
+            {
+                //label1.Location = new Point(e.GetPosition(null).X + rnd.Next(-10, 10), e.GetPosition(null).Y);
+                btn.Margin = new Thickness(btn.Margin.Left + 20, btn.Margin.Top, 0, 0);
+
+            }
+            //if ((e.GetPosition(null).X <= 0 || e.GetPosition(null).X >= btn.Margin.Left+10) && (e.GetPosition(null).Y <= 0 || e.GetPosition(null).Y >= btn.Margin.Top + 10))
+           // {
+               // MessageBox.Show("You Catch me ;(", "It`s all");
+           // }
+
+        }
+
+
+    }
+}
